4d8c1627aa26212fba8c8a772a10b74e7926e0e0
SAVAGE <proj_1>
2023-04-04 16:15:38 +0300

    job_done

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿Main();
+
+void Main()
+{
+	bool isWorking = true;
+
+	while (isWorking)
+	{
+		Console.WriteLine("Input task");
+
+		string task = Console.ReadLine();
+
+		switch (task)
+		{
+			case "exit": isWorking = false; break;
+			case "t1": Task1(); break;
+			default:
+				break;
+		}
+	}
+}
+
+void Task1()
+{
+	int a = ReadInt("a");
+	int b = ReadInt("b");
+	int less = a;
+
+	if (IsMoreThen(a, b))
+	{
+		less = b;
+	}
+
+	Console.WriteLine(less);
+}
+
+int ReadInt(string argument)
+{
+	int number;
+
+	Console.Write($"Input {argument}:");
+
+	while (!int.TryParse(Console.ReadLine(), out number))
+	{
+		Console.WriteLine("It's not a number!");
+	}
+
+	return number;
+}
+
+bool IsMoreThen(int a, int b)
+{
+	return a > b;
+}
